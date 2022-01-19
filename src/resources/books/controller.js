df1ebb738e12702d192ca5d1778f04954e3fb039
Carlo M <carlo992@gmail.com>
2022-01-19 16:38:16 +0100

    Added some data validation in posting a book and returning meaningful error messages

--- src/resources/books/controller.js
+++ src/resources/books/controller.js
@@ -6,17 +6,52 @@ async function createOne(req, res) {
     ...req.body,
   }
 
+  if(bookToCreate.title === undefined) {
+    return res.status(500).send({error: "Book title was not provided"});
+  }
+  if(bookToCreate.type === undefined) {
+    return res.status(500).send({error: "Book type was not provided"});
+  }
+  if(bookToCreate.author === undefined) {
+    return res.status(500).send({error: "Book author was not provided"});
+  }
+  if(bookToCreate.topic === undefined) {
+    return res.status(500).send({error: "Book topic was not provided"});
+  }
+  if(bookToCreate.publicationDate === undefined) {
+    return res.status(500).send({error: "Book publicationDate was not provided"});
+  }
+  else {
+    try {
+      const convertedDate = new Date(bookToCreate.publicationDate)
+      if(isNaN(convertedDate)) throw Error("Invalid date")
+    }
+    catch(err) {
+      return res.status(500).send({error: "Book publicationDate is not a valid date:" + bookToCreate.publicationDate});
+    }
+  }
+
   console.log("Attempting to create book:", bookToCreate)
-  let result = await BookModel.createBook(bookToCreate)
+  const result = await BookModel.createBook(bookToCreate)
   console.log("Book creation response:", result)
-  res.json({data: result})
+  if(result.error !== undefined) {
+    res.status(500).send(result)
+  }
+  else {
+    res.json({data: result})
+  }
 }
 
 async function getAll(req, res) {
   console.log("Attempting to get all books")
-  let result = await BookModel.getAllBooks()
+  const result = await BookModel.getAllBooks()
   console.log("Fetching books response:", result)
-  res.json({data: result})
+  if(result.error !== undefined) {
+    res.status(500).send(result)
+  }
+  else {
+    res.json({data: result})
+  }
 }
 
 module.exports = {createOne, getAll}
